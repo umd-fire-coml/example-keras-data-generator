d55a4712cdd7c7d2e19af4efff3f30c2e62a76ed
Allen Tu <atu1@umd.edu>
2021-09-28 16:29:46 -0400

    Fix tests

--- test/test_datagen.py
+++ test/test_datagen.py
@@ -9,13 +9,13 @@ def my_data_generator():
     return DataGenerator()
 
 def test_batch_size():
-    assert len(my_data_generator().__getitem__(1)[0]) == my_data_generator().batch_size
+    generator = my_data_generator()
+    assert(len(generator.__getitem__(1)[0]) == generator.batch_size)
 
 def test_x_shape():
-    assert np.shape(my_data_generator().__getitem__(1)[0][0]) == my_data_generator().x_shape
+    generator = my_data_generator()
+    assert(np.shape(generator.__getitem__(1)[0][0]) == generator.x_shape)
 
 def test_y_shape():
-    assert np.shape(my_data_generator().__getitem__(1)[1][0]) == my_data_generator().y_shape
-
-def test_num_dataset_items():
-    assert 1==1
+    generator = my_data_generator()
+    assert(np.shape(generator.__getitem__(1)[1][0]) == generator.y_shape)
