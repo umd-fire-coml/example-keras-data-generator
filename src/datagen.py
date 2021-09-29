11e5106857eaa29a64614054fc0f40f07d2c6c7e
Mariel Viray <rviray@terpmail.umd.edu>
2021-09-29 14:49:52 -0400

    Image Generator

--- src/datagen.py
+++ src/datagen.py
@@ -6,17 +6,16 @@ from tensorflow.keras.utils import Sequence
 class DataGenerator(Sequence):
     '''this is a random data generator, edit this data generator to read data from dataset folder and return a batch with __getitem__'''
 
-    def __init__(self, batch_size=8, x_shape=(256, 256, 3), y_shape=(10,), n_dataset_items=100):
+    def __init__(self, batch_size=8, x_shape=(360, 480, 3), y_shape=(1,), n_dataset_items=100):
         self.batch_size = batch_size
         self.x_shape = x_shape
         self.y_shape = y_shape
         self.n_dataset_items = n_dataset_items
         self.indexes = np.arange(self.n_dataset_items)
-   
-   
+       	self.on_epoch_end()
         self.x_filepaths = os.listdir('./test/data/images')
         self.y_labels = np.genfromtxt(./test/data/image_labels.txt', dtype='int')
-    	self.on_epoch_end()
+
 
     def __len__(self):
         """Denotes the number of batches per epoch
