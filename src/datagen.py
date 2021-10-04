a22059908b1c3e920a4cf5c48d57f0f374b508a7
Varun Ramani <varun.ramani@gmail.com>
2021-10-04 15:09:14 -0400

    Finished

--- src/datagen.py
+++ src/datagen.py
@@ -1,26 +1,31 @@
 import numpy as np
+from numpy.core.numeric import indices
 from tensorflow.keras.utils import Sequence
+import cv2
+import os
+import pathlib
 
 class DataGenerator(Sequence):
     '''this is a random data generator, edit this data generator to read data from dataset folder and return a batch with __getitem__'''
 
-    def __init__(self, batch_size=8, x_shape=(256, 256, 3), y_shape=(10,), n_dataset_items=100):
+    def __init__(self, batch_size=8, x_shape=(360, 480, 3), y_shape=(1,)):
         self.batch_size = batch_size
+
         self.x_shape = x_shape
         self.y_shape = y_shape
-        self.n_dataset_items = n_dataset_items
-        self.indexes = np.arange(self.n_dataset_items)
+
+        self.X = [(pathlib.Path('test/data/images') / filename).as_posix() for filename in os.listdir('test/data/images')]
+        self.y = np.genfromtxt('test/data/image_labels.txt')
+
+        self.indices = np.arange(len(self.X))
+
         self.on_epoch_end()
         
-        # DELETE THIS WHEN USING YOUR OWN DATASET, DO NOT STORE THE ACTUAL DATASET IN MEMEORY HERE
-        self.X_DATASET = [np.random.rand(*self.x_shape) for i in range(self.n_dataset_items)] 
-        self.Y_DATASET = [np.random.rand(*self.y_shape) for i in range(self.n_dataset_items)]
-
     def __len__(self):
         """Denotes the number of batches per epoch
         :return: number of batches per epoch
         """
-        return int(np.floor(self.n_dataset_items / self.batch_size))
+        return len(self.indices) // self.batch_size
 
     def __getitem__(self, index):
         """Generate one batch of data
@@ -28,24 +33,21 @@ class DataGenerator(Sequence):
         :return: x_batch and y_batch
         """
         # Initialization
-        x_batch = np.empty((self.batch_size, *self.x_shape))
-        y_batch = np.empty((self.batch_size, *self.y_shape))
+        batch_X = np.empty((self.batch_size, *self.x_shape))
+        batch_y = np.empty((self.batch_size, *self.y_shape))
 
         # Generate indexes of the batch
-        indexes = self.indexes[index * self.batch_size : (index + 1) * self.batch_size]
+        indexes = self.indices[index * self.batch_size : (index + 1) * self.batch_size]
+        for i, data_index in enumerate(indexes):
+            batch_X[i] = cv2.imread(self.X[data_index])
+            batch_y[i] = self.y[data_index]
 
-        # Generate data
-        for i in range(self.batch_size):
-            x_batch[i,] = self.X_DATASET[indexes[i]]
-            y_batch[i,] = self.Y_DATASET[indexes[i]]
-
-        # Return batch data
-        return x_batch, y_batch
-
-
+        return batch_X, batch_y
 
     def on_epoch_end(self):
         """Shuffle indexes after each epoch
         """
-        np.random.shuffle(self.indexes)
+        np.random.shuffle(self.indices)
 
+generator = DataGenerator()
+print(generator.X)
